7ae2fa12456a77e8a698745cfc54ec1d4c52c081
Felipe Silva <felipeo.silva@hotmail.com>
2022-04-03 02:38:08 -0300

    Add cors dependency to make requests from bot

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// API Routes
 const locationFunc = require('./routes/v1/location/:zipcode/func.js')
 
+app.use(
+  cors({
+      credentials: true,
+      origin: true
+  })
+);
+app.options('*', cors());
+
+app.get('/', (req, res) => res.send('Alive!!!'));
 app.get('/v1/location/:zipcode', locationFunc);
 
-try {
-  app.listen(3000)
-  console.log("Server started on port 3000")
-} catch(error) {
-  console.log("Error to start server")
-  console.error(error)
-}
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server running on port 3000', '');
+});

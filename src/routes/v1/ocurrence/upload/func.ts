475fa405e76ef7ee3e063a9dcd6927c9792e28fc
Felipe Tiozo <felipetiozo10@gmail.com>
2022-04-03 23:36:16 -0300

    Integrates upload route with AI code

--- src/routes/v1/ocurrence/upload/func.ts
+++ src/routes/v1/ocurrence/upload/func.ts
@@ -1,7 +1,10 @@
-import path from 'path'
+import FormData from 'form-data'
 import BadRequest from '@app/errors/BadRequest'
-import { upload } from '@app/helpers/s3'
 import Ocurrence from '@app/models/ocurrence'
+import User from '@app/models/user'
+import Axios from 'axios'
+
+const WENI_FLOW = '83e9a53c-6c44-46e7-81f6-26a7992647a8'
 
 export default async function func(req, res) {
   try {
@@ -26,36 +29,48 @@ export default async function func(req, res) {
       throw new BadRequest(`Invalid uploadToken or userId`)
     }
 
+    const user = await User.findOne({
+      _id: userId,
+    })
+
+    if (!user) {
+      throw new BadRequest(`Invalid userId`)
+    }
+
     // Verify file is present
     if (!file) {
       throw new BadRequest(`'file' must be present in form-data request`)
     }
 
-    const ext = path.extname(file.originalname)
-
-    const validExtensions = ['.jpeg', '.jpg', '.png']
-
-    if (!validExtensions.includes(ext)) {
-      throw new BadRequest(`Invalid extension: ${ext}`)
-    }
-
-    // eslint-disable-next-line no-async-promise-executor
-    const uploadedFilePromise = new Promise(async (resolve, reject) => {
-      try {
-        const uploadPromise = await upload(
-          file.buffer,
-          `upload-${userId}-${uploadToken}`,
-          ext
-        )
-        resolve(uploadPromise)
-      } catch (err) {
-        reject(err)
+    const bodyFormData = new FormData()
+    bodyFormData.append('image', req.file.buffer, 'image')
+    const { data } = await Axios.post(
+      'http://34.219.39.10:5000/model',
+      bodyFormData,
+      {
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${bodyFormData.getBoundary()}`,
+        },
       }
-    })
+    )
 
-    const uploadedFile: any = await uploadedFilePromise
+    await Axios.post(
+      'https://new.push.al/api/v2/flow_starts.json',
+      {
+        flow: WENI_FLOW,
+        urns: [`whatsapp:${user.whatsapp}`],
+        params: {
+          type: data.split(' ')[0],
+        },
+      },
+      {
+        headers: {
+          Authorization: process.env.WENI_TOKEN,
+        },
+      }
+    )
 
-    res.send({ url: uploadedFile.Location })
+    res.send({ success: true })
   } catch (error) {
     const status = error.statusCode || 500
     res.status(status)
